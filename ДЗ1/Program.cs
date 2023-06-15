cef206f5b4300559c550cea02dd1d2c8e1bebc81
Maksim19B83 <135154401+Maksim19B83@users.noreply.github.com>
2023-06-16 01:31:20 +0300

    Add files via upload

---
+++ ДЗ1/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 2
+// Console.WriteLine("Введите число А: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число В: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// if (A>B)
+// {
+//     System.Console.WriteLine("max = {0}", A);
+// }
+// else  if (A<B)
+//     System.Console.WriteLine("max = {0}", B);
+// else
+//     System.Console.WriteLine("Числа равны");
+
+// Задача 4
+// Console.WriteLine("Введите число А: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число C: ");
+// int C = Convert.ToInt32(Console.ReadLine());
+// int max;
+// if (A>=B)
+// { 
+//    if (C>=A) 
+//    {
+//        max = C;
+//        System.Console.WriteLine("max = {0}", C);
+//    }
+//    else
+//    {
+//        max = A;
+//        System.Console.WriteLine("max = {0}", A);
+//    }   
+// }
+// else
+// {
+//     if (C>=B)
+//     {
+//     max = C;
+//     System.Console.WriteLine("max = {0}", C);
+//     }
+//     else
+//     { 
+//     max = B;
+//     System.Console.WriteLine("max = {0}", B);
+//     }
+// }
+// Задача 6
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int pok;
+// pok=num%2;
+// if (pok==0)
+// {
+//     System.Console.WriteLine("Да");
+// }
+// else
+//     System.Console.WriteLine("Нет");
+// Задача 8
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int count = 2;
+
+// while (count<=num)
+// {
+//     System.Console.Write(count+ ", ");
+//     count=count+2;
+// }
